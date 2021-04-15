af31ba460d0282cfd66b70a4db51fdbf1d023c79
huguangyuan <760229204@qq.com>
2021-04-15 19:10:48 +0800

    导航初步设置

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.nav-bar{
+  display: flex;
+  /* 必须设置高度，没有内容撑不起来 */
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+}
+.left, .right{
+  width: 60px;
+}
+.center{
+  flex:1;
+}
+</style>
